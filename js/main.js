63df01b062190ca400324759be40416a915bb965
Andresvn <carlos.valencia10@outlook.com>
2018-09-23 10:23:43 -0500

    version 2.0

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 function station(){
-       url_link = "http://estaciont-backend.herokuapp.com/api/v1/stations";
-        var jqxhr = $.get( url_link, function() {
-          alert( "success" );
-        })
-          .done(function() {
-            alert( "second success" );
-          })
-          .fail(function() {
-            alert( "error" );
-          })
-          .always(function() {
-            alert( "finished" );
-          });
+       $.get( "http://estaciont-backend.herokuapp.com/api/v1/stations", function(data) {
+          console.log('sucess',data);
+          first_station= data[1];
+          $("#current_station").html(first_station.name);
+          $("#station_img").attr("src",first_station.image_url);
+
+          // $("#station_img").css('transform','rotate('+90+'deg)');
+          console.log(first_station.id);
+          // $(v).each(function(index){
+          //   console.log( index );
+          // })
+         // $.get("#body-alarm").html(v[0]);
+        });
+}
+
+function alerts(){
+       $.get( "http://estaciont-backend.herokuapp.com/api/v1/stations/1/alerts", function(data) {
+          console.log('sucess',data);
+          last_alarm= data[data.length-1];
+          $("#body-title").html("ID: "+ last_alarm.id);
+          $("#alarms-description").html(last_alarm.description);
+          $("#id-alert").html("ID: "+ last_alarm.id);
+          $("#description-alert").html( last_alarm.description);
+          // $("#station_img").css('transform','rotate('+90+'deg)');
+          console.log(last_alarm);
+          // $(v).each(function(index){
+          //   console.log( index );
+          // })
+         // $.get("#body-alarm").html(v[0]);
+        });
 }
 
 $(document).ready(function() {
     station();
+    alerts();
+
 })
